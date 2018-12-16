5359d88848a2f366c3f8bc26866d196ee6b0da37
Didqh <didacatz@hotmail.com>
2018-12-16 11:54:01 +0100

    Add an example ¿How to give custom items?

--- Examples/src/Examples/ExamplesPlayer.php
+++ Examples/src/Examples/ExamplesPlayer.php
@@ -7,6 +7,7 @@ namespace Examples;
 
 use pocketmine\item\Item;
 use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 class ExamplesPlayer extends Player {
 
@@ -35,6 +36,31 @@ class ExamplesPlayer extends Player {
         for($i = 0; $i < (36 - count($inv->getContents())); $i++) {
             $inv->addItem(Item::get(Item::BAKED_POTATO));
         }
+    }
+
+    /**
+     * @param Item $item
+     * @param string $name
+     * @param string[] $lore
+     */
+    public function addCustomItem(Item $item, string $name, array $lore): void {
+        $item->setCustomName($name);
+        $item->setLore($lore);
+        $this->getInventory()->addItem($item);
+    }
+
+    /**
+     * This is the example in above but with no parameters
+     */
+    public function addDefaultCustomItem(): void {
+        $item = Item::get(Item::MOB_HEAD);
+        $item->setCustomName(TextFormat::GOLD . "Default Head");
+        $item->setLore([
+            TextFormat::DARK_AQUA . "This is a lore example.",
+            "", // Empty line
+            "Another example"
+        ]);
+        $this->getInventory()->addItem($item);
     }
 
 }
